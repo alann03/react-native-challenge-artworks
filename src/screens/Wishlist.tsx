19bb7d44ac83c200a5ba10693852b2814a818d04
alann03 <alanhuismann03@gmail.com>
2025-02-24 15:03:41 -0300

    add empty message to wishlist

--- src/screens/Wishlist.tsx
+++ src/screens/Wishlist.tsx
@@ -1,10 +1,27 @@
 import { useWishlist } from "../contexts/WishlistProvider";
 import List from "../components/List";
+import { StyleSheet, View } from "react-native";
 
 const WishlistScreen = () => {
   const { wishlist } = useWishlist();
 
-  return <List data={wishlist} handleNextPage={() => {}} isLoading={false} isPaginating={false} />;
+  return (
+    <View style={styles.container}>
+      <List
+        data={wishlist}
+        handleNextPage={() => {}}
+        isLoading={false}
+        isPaginating={false}
+        emptyMessage="Your wishlist is empty"
+      />
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default WishlistScreen;
